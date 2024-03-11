cc4529f3ca1ff467601da1995e7162502701f191
Megira <regenerat.55@mail.ru>
2024-03-11 10:15:15 +0300

    расширил класс User от IdentityInterface
    добавил функцию для проверки логина и пароля

--- models/User.php
+++ models/User.php
@@ -18,7 +18,7 @@ use Yii;
  * @property Report[] $reports
  * @property Role $role
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -56,6 +56,16 @@ class User extends \yii\db\ActiveRecord
         ];
     }
 
+    public static function login($login, $password) {
+        $user = static::find()->where(['login' => $login])->one();
+
+        if($user && $user->validatePassword($password)) {
+            return $user;
+        }
+
+        return null;
+    }
+
     /**
      * Gets query for [[Reports]].
      *
@@ -74,5 +84,56 @@ class User extends \yii\db\ActiveRecord
     public function getRole()
     {
         return $this->hasOne(Role::class, ['id' => 'role_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return static::find()->where(['id' => $id])->one();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return null;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
     }
 }
